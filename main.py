2071f44d8865ad5982685b914a1cc86599aa1e2e
KasperJuunge <kasperjuunge@gmail.com>
2022-11-18 23:13:34 +0100

    .

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import pandas as pd
 from notebooks.mc4 import download_n_articles_from_url
 
 df = download_n_articles_from_url(n=100000000, url_pattern="nyheder.tv2.dk")
-df.to_csv("./data/news2.csv")
+df.to_csv("./data/news3.csv", index=False)
