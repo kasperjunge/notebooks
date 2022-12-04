f35bf2e81383749a62bef7c1631c2204e8afbef8
KasperJuunge <kasperjuunge@gmail.com>
2022-12-04 22:06:38 +0100

    .

--- src/notebooks/strings/preprocessing.py
+++ src/notebooks/strings/preprocessing.py
@@ -1,3 +1,17 @@
+import re
+from typing import List
+
 
 class SentenceTokenizer:
-    ...
+    
+    def __call__(self, text: str) -> List[str]:
+        sentences = text.split(".")
+        sentences = [sentence.strip()+"." for sentence in sentences]
+        return sentences
+
+
+class StringCleaner:
+
+    def __call__(self, text: str) -> str:
+        text = re.sub(" +", " ", text)
+        return text
